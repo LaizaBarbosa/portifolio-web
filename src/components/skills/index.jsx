749de5121f66dd5d00755b00a11f529b99d87ac3
LaizaBarbosa <laiza0700@gmail.com>
2024-07-04 12:44:59 -0300

    feat: criado seção de skills

---
+++ src/components/skills/index.jsx
@@ -0,0 +1,26 @@
+import './style.css'
+import iconeReact from '../../assets/icon-react.svg'
+
+
+// react, js, css, html, tailwind, axios
+// Node, Java, MySQl, MongoDB, Sequelize, Express
+
+export default function Skills(){
+    return(
+        <article id="skills">
+            <h1>Skills</h1>
+            <section className="stack">
+                <h2>Front-End</h2>
+                <div className="tecnologias">
+
+                </div>
+            </section>
+            <section className="stack">
+                <h2>Back-End</h2>
+                <div className="tecnologias">
+                    
+                </div>
+            </section>
+        </article>
+    )
+}
